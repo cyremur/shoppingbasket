fb15258788c9c1d286e4155a0235c1b659da54a5
je11 <je@newlife.ai>
2023-04-28 19:45:22 +0200

    add products and pricing logic

--- src/server/api/routers/example.ts
+++ src/server/api/routers/example.ts
@@ -1,6 +1,9 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import productJson from '../../../content/products.json'
+
+
 
 export const exampleRouter = createTRPCRouter({
   hello: publicProcedure
@@ -10,4 +13,10 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
+    getProducts: publicProcedure
+      .query(() => {
+        return {
+          productJson
+        };
+      }),
 });
